9ac05b87352651363cd5f88c923fa840c071e461
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-11-29 11:08:42 +0600

    my-buyers page functionality addeed

--- src/Pages/Dashboard/SellerDashboard/MyBuyers.js
+++ src/Pages/Dashboard/SellerDashboard/MyBuyers.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../../../Context/UserContext';
 
 const MyBuyers = () => {
+    const [buyers, setBuyers] = useState([]);
+    const { user } = useContext(AuthContext);
+    const sellerEmail = user?.email;
+
+    fetch(`http://localhost:5000/mybuyer/${sellerEmail}`)
+        .then(res => res.json())
+        .then(data => setBuyers(data))
+    // console.log(buyers);
+
+
+
     return (
         <div>
-            <h1>this is my buyers page</h1>
+            <h1 className='text-5xl font-bold text-green-500 text-center my-12'>My Buyers</h1>
+            <div className="overflow-x-auto w-full px-5">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Buyers Name</th>
+                            <th>Buyers Email</th>
+                            <th>Items Purchased</th>
+                            <th>Total Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            buyers.map(buyer => (
+                                <tr key={buyer._id}>
+                                    <td>{buyer.buyerName}</td>
+                                    <td>{buyer.buyerEmail}</td>
+                                    <td>{buyer.productName}</td>
+                                    <td>{buyer.productPrice}$</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
